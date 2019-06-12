28586001c405376fb632b16ffa7c498a20de2ba6
Alex Kopen <alexkopen@gmail.com>
2019-06-12 13:51:59 -0600

    Stripped previous site content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
-import {NavigationStart, Router} from '@angular/router';
-import {isPlatformBrowser} from "@angular/common";
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  constructor(@Inject(PLATFORM_ID) private platformId: Object, private router: Router) {}
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.router.events.subscribe(value => {
--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HomeSlide } from '../../shared/models/home-slide.model';
 
 @Component({
   selector: 'app-banner',
@@ -7,15 +6,6 @@ import { HomeSlide } from '../../shared/models/home-slide.model';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent implements OnInit {
-  homeSlide = new HomeSlide(
-    'Our Call to Serve',
-    'Since 1996, Reiser Relief has been providing relief, hope, and dignity to the poor, elderly, and homeless people of Haiti.',
-    'impact-slide.jpg',
-    'Support Us',
-    '/',
-    'Give Today'
-  );
-
   constructor() {}
 
   ngOnInit() {}
